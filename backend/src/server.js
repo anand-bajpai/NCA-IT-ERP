94726b8366fec7c9f7fb36fbd779b9ce5e2c68fc
Anand Bajpai <149421767+anand-bajpai@users.noreply.github.com>
2026-07-24 17:33:04 +0530

    Update allowed origins for CORS

--- backend/src/server.js
+++ backend/src/server.js
@@ -52,7 +52,7 @@ app.use(cookieParser());
 
 const allowedOrigins = (
   process.env.CLIENT_ORIGIN ||
-  "http://localhost:5173,https://nca-it-8ty68bqz3-anand-bajpai1.vercel.app"
+  "http://localhost:5173,https://nca-it-erp-eight.vercel.app"
   
 )
   .split(",")
